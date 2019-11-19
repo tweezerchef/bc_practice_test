e7aeef370cd929d034ea67134deb022f82a84f1d
Kaelyn Chresfield <kaelyn.chresfield@gmail.com>
2019-11-19 12:58:11 -0600

    created test with spec, readme, and js prompts

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # bc_pratice_test
-Practice exam for Bootcamp Class
+Welcome to the 1st Annual Alien Dog Show!
+
+Since this is our first event, we're hiring jr Developers like yourself to make sure things don't go wrong.
+Let's get started!
+
+
+1. Since this is an intergalatic event, many of our patrons speak different languages!
+    Assign a function to our our `greeting` variable that takes 3 inputs: `greeting`, `location` and `time`. Concatanate these values together to form a viable sentence and log it to the console.
+
+2. We have more contestants than anticipated. (We actually don't have a count!)
+    Assign a function to our `contestants` variable that will return the number of entries in our dogs array. 
+
+
+3. Oh no! Some of our contestants aren't even dogs!
+    Using the native filter method with our `dogs` array, create a function called `filterSpecies` that will filter out all of the species that are not dogs! 
+
+4. For safty purposes, assign a copy of our `filteredSpecies` array to out `dogContestants` variable, using the spread operator.
+
+5. Now that we have our `dogContestants` array, we need to add a `class` property to each dog object.
+    Using the native map method, add a key of `class` with the value:
+        - "red" if the dog's weight is between 1 and 10.
+        - "yellow" if the dog's weight is between 11 and 20.
+        - "green" if the dog's weight is over 20.
+
+
+6. The votes are in! We have an array of the top dogs in each class
+    Using recursion, copy all of the properties into one object and return that object.
+
+
+7. For statistical reasons, lets count all of the votes we recieved for our dog and non-dog contestants!
+    Using reduce, find the sum of the votes casted.
---
+++ index.js
@@ -0,0 +1,120 @@
+// const dogs = require('./dogData');
+let dogs = [
+    {
+        "name": "Doggo-co-WOOF",
+        "age": 3,
+        "species": "dog",
+        "weight": 45,
+        "votes": 22
+    }, {
+        "name": "Space CATdet",
+        "age": 4,
+        "species": "cat",
+        "weight": 20,
+        "votes": 5
+    }, {
+        "name": "Solar Squirrel",
+        "age": 1,
+        "species": "squirrel",
+        "weight": 0.5,
+        "votes": 13
+    }, {
+        "name": "Bark Hole",
+        "age": 20,
+        "species": "dog",
+        "weight": 19,
+        "votes": 20
+    }, {
+        "name": "Howlling Winds",
+        "age": 55,
+        "species": "dog",
+        "weight": 90,
+        "votes": 200
+    }, {
+        "name": "Electron Nutria",
+        "age": 7,
+        "species": "nutria",
+        "weight": 3,
+        "votes": 0
+    }, {
+        "name": "Howlling Winds Sr.",
+        "age": 59,
+        "species": "dog",
+        "weight": 101,
+        "votes": 201
+    }, {
+        "name": "Tycho Brahe",
+        "age": 35,
+        "species": "moose",
+        "weight": 500,
+        "votes": 123
+    }, {
+        "name": "Johannes Kepler",
+        "age": 9,
+        "species": "dog",
+        "weight": 5,
+        "votes": 55
+    }, {
+        "name": "Pluto",
+        "age": 89,
+        "species": "dog",
+        "weight": 10,
+        "votes": 190
+    }, {
+        "name": "Quark",
+        "age": 20,
+        "species": "dog",
+        "weight": 1,
+        "votes": 45
+    }, {
+        "name": "Higgs",
+        "age": 26,
+        "species": "dog",
+        "weight": 0.00001,
+        "votes": 200
+    }, {
+        "name": "Lepton",
+        "age": 12,
+        "species": "dog",
+        "weight": 15,
+        "votes": 175
+    }, {
+        "name": "Boson",
+        "age": 9,
+        "species": "dog",
+        "weight": 8,
+        "votes": 141
+    }
+]
+
+// 1.
+var greeting;
+
+
+// 2.
+var contestants;
+
+
+// 3.
+var filterSpecies;
+
+
+// 4. 
+var dogContestants;
+
+
+
+// 5. 
+var dogsWithClasses;
+
+    
+
+
+// 6.
+var firstInClass;
+
+
+
+
+// 7.
+var votes;
